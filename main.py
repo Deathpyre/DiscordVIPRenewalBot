32eddb06cd573dcaa08a37d3aac22cc79d015c16
Josh Armour <deathpyre@gmail.com>
2025-05-06 03:25:45 -0400

    feeer

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ intents.message_content = True
 intents.members = True
 intents.webhooks = True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = Bot(token)#commands.Bot(command_prefix='!', intents=intents)
 
 admin_role = "admin"
 vip_role = "⛥ Saints ⛥"
@@ -31,11 +31,11 @@ keep_alive()
 #             return member
 #     return None
 
-@bot.event
-async def on_ready():
-   print(f'Logged in as {bot.user.name} - {bot.user.id}')
-   print('------')
-   # await bot.change_presence(status=discord.Status.online, activity=discord.Game(name='Elite Saints In World'))#name='Elite Saints VIP In World'))
+# @bot.event
+# async def on_ready():
+#    print(f'Logged in as {bot.user.name} - {bot.user.id}')
+#    print('------')
+#    await bot.change_presence(status=discord.Status.online, activity=discord.Game(name='Elite Saints In World'))#name='Elite Saints VIP In World'))
 
 bot.run(token)#, log_handler=handler, log_level=logging.DEBUG
 
