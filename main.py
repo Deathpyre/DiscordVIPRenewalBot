3a7b1af36f83494771002bb4cbaa2846c640eca5
Deathpyre <59452397+Deathpyre@users.noreply.github.com>
2025-05-07 02:03:48 -0400

    Update main.pyaaaaaaaaaa

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 ﻿#from xml.sax import handler
 import discord
 from discord.ext import commands
-#import logging
+import logging
 #from dotenv import load_dotenv
 import os
 import requests
@@ -12,11 +12,11 @@ import requests
 #load_dotenv()
 token = os.environ.get('DISCORD_TOKEN')
 
-#handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
-intents.webhooks = True
+#intents.webhooks = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)#discord.Client(intents=intents)#
 
@@ -24,11 +24,11 @@ admin_role = "admin"
 vip_role = "⛥ Saints ⛥"
 elite_role = "⛥ Elite Saints ⛥"
 
-bot.run(token)#, log_handler=handler, log_level=logging.DEBUG)
+bot.run(token, log_handler=handler, log_level=logging.DEBUG)
 
 #from keep_alive import keep_alive
 #keep_alive()
-app = Flask(__name__)
+#app = Flask(__name__)
 #
 #@app.route('/')
 #def hello():
