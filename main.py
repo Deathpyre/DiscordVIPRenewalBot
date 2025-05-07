cf95e3bc6ecc8ed866bc1665d46bcbf9c2355e8b
Deathpyre <59452397+Deathpyre@users.noreply.github.com>
2025-05-07 00:50:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -32,9 +32,9 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
-#if __name__ == '__main__':
-#        port = int(os.environ.get('PORT', 4000))
-#        app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+        port = int(os.environ.get('PORT', 4000))
+        app.run(host='0.0.0.0', port=port)
 
 def username_to_member(guild: discord.Guild, name:str):
     "Returns None if name not found in guild"
